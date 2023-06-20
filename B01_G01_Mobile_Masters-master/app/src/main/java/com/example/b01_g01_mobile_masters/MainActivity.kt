dd656702be1d43c18b518152dee780e15f839b7b
manjitha-dev <55598831+manjitha-dev@users.noreply.github.com>
2023-06-20 17:36:04 +0530

    Add files via upload

---
+++ B01_G01_Mobile_Masters-master/app/src/main/java/com/example/b01_g01_mobile_masters/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.b01_g01_mobile_masters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
